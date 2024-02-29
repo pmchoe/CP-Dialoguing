0c9a9b87c675ab7f132ee130b6ee842ba873333a
Philip Choe <130533114+pmchoe@users.noreply.github.com>
2024-02-28 23:55:12 -0800

    started and finished dialoguing exercise

---
+++ src/scenes/Title.js
@@ -0,0 +1,41 @@
+class Title extends Phaser.Scene {
+    constructor() {
+        super("titleScene")
+    }
+
+    preload() {
+        // load assets
+        this.load.path = "./assets/"
+
+        // load JSON (ie dialog text)
+        this.load.json('dialog', 'json/dialog.json')
+
+        // load images
+        this.load.image('dialogbox', 'img/dialogbox.png')
+        this.load.image('homer', 'img/homer.png')
+        this.load.image('minerva', 'img/minerva.png')
+        this.load.image('jove', 'img/jove.png')
+        this.load.image('neptune', 'img/neptune.png')
+        this.load.image('pomni', 'img/pomni.png')
+
+        // load bitmap font
+        this.load.bitmapFont('gem_font', 'font/gem.png', 'font/gem.xml')
+        this.load.bitmapFont('bubbleeKids_font', 'font/bubbleeKids.png', 'font/bubbleeKids.xml')
+    }
+
+    create() {
+        // add title text
+        this.add.bitmapText(centerX, centerY - 32, 'bubbleeKids_font', 'THE ODYSSEY', 64).setOrigin(0.5)
+        this.add.bitmapText(centerX, centerY, 'bubbleeKids_font', 'Press SPACE to start', 24).setOrigin(0.5)
+
+        // create input
+        cursors = this.input.keyboard.createCursorKeys()
+    }
+
+    update() {
+        // wait for player input
+        if(Phaser.Input.Keyboard.JustDown(cursors.space)) {
+            this.scene.start("talkingScene")
+        }
+    }
+}
